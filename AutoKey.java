7aa48120cd355466724b1dcc80c90db8d6c6202f
relac002 <145077114+relac002@users.noreply.github.com>
2024-04-24 17:47:48 -0500

    Code project finished

---
+++ AutoKey.java
@@ -0,0 +1,143 @@
+//Wendell Relacion
+//AutoKey cipher implementation project.
+public class AutoKey {
+
+	private static final String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	public static void main(String[] args){
+		String msg = "HELLOIWOULDVERYMUCHLIKESOMEMORETIME";
+		String key = "NLP";
+
+		if (key.matches("[-+]?\\d*\\.?\\d+"))
+			key = "" + alphabet.charAt(Integer.parseInt(key));
+		
+
+		System.out.println("Plaintext : " + msg);
+		System.out.println("Key : " + key);
+
+		System.out.println();
+		String enc = autoEncryption(msg, key);
+			
+		// System.out.println("Plaintext : " + msg);
+		System.out.println("Encrypted : " + enc);
+		System.out.println();
+		System.out.println("Decrypted : " + autoDecryption(enc, key));
+	}
+
+	public static String autoEncryption(String msg, String key){
+		System.out.println("Start of Encryption (subtraction)");
+		int len = msg.length();
+
+		// generating the keystream
+		String newKey = key.concat(msg);
+		System.out.println("New Key : " + newKey);
+		newKey = newKey.substring(0, newKey.length() - key.length());
+		String encryptMsg = "";
+
+		// applying algorithm
+		System.out.print("Plain Text          :  ");
+		for (int x = 0; x < len; x++) {
+			int first = alphabet.indexOf(msg.charAt(x));
+			int second = alphabet.indexOf(newKey.charAt(x));
+			int total = (first + second) % 26;
+			System.out.print(msg.charAt(x) + "  ");
+			
+			encryptMsg += alphabet.charAt(total);
+		}
+		System.out.println();
+
+		//print out corresponding number
+		System.out.print("corresponding number: ");
+		for (int x = 0; x < len; x++) {
+			int first = alphabet.indexOf(msg.charAt(x));
+			System.out.print(first + "  ");
+		}
+		System.out.println();
+
+		System.out.print("New Key             :  ");
+		for (int x = 0; x < newKey.length(); x++) {
+
+			System.out.print(newKey.charAt(x) + "  ");
+
+		}
+		System.out.println();
+
+		System.out.print("Corresponding number: ");
+		for (int x = 0; x < newKey.length(); x++) {
+
+			int second = alphabet.indexOf(newKey.charAt(x));
+			System.out.print(second + "  ");
+
+		}
+		System.out.println();
+
+		System.out.print("Formula number      : ");
+		for (int x = 0; x < newKey.length(); x++) {
+
+			int second = alphabet.indexOf(encryptMsg.charAt(x));
+			System.out.print(second + "  ");
+
+		}
+		System.out.println();
+
+		System.out.print("Encrypted String    : ");
+		for (int x = 0; x < newKey.length(); x++) {
+
+			System.out.print(encryptMsg.charAt(x) + "   ");
+
+		}
+		System.out.println();
+		return encryptMsg;
+	}
+
+	public static String autoDecryption(String msg, String key){
+		System.out.println("Start of Decryption (subtraction)");
+		int len = msg.length();
+		String currentKey = key;
+		String decryptMsg = "";
+
+		System.out.print("Text to Decrypt     :  ");
+		for (int x = 0; x < msg.length(); x++) {
+			System.out.print(msg.charAt(x) + "  ");
+			int get1 = alphabet.indexOf(msg.charAt(x));
+			int get2 = alphabet.indexOf(currentKey.charAt(x));
+			int total = (get1 - get2) % 26;
+			total = (total < 0) ? total + 26 : total;
+			decryptMsg += alphabet.charAt(total);
+			currentKey += alphabet.charAt(total);
+		}
+		System.out.println();
+
+		System.out.print("corresponding number: ");
+		for (int x = 0; x < len; x++) {
+			int first = alphabet.indexOf(msg.charAt(x));
+			System.out.print(first + "  ");
+		}
+		System.out.println();
+
+		System.out.print("New Key             :  ");
+		for (int x = 0; x < msg.length(); x++) {
+			System.out.print(currentKey.charAt(x) + "  ");
+
+		}
+		System.out.println();
+
+		System.out.print("corresponding Number:  ");
+		for (int x = 0; x < msg.length(); x++) {
+			int get2 = alphabet.indexOf(currentKey.charAt(x));
+			System.out.print(get2 + "  ");
+
+		}
+		System.out.println();
+
+		System.out.print("Formula number      :  ");
+		for (int x = 0; x < decryptMsg.length(); x++) {
+
+			int second = alphabet.indexOf(decryptMsg.charAt(x));
+			System.out.print(second + "  ");
+
+		}
+		System.out.println();
+		return decryptMsg;
+	}
+}
